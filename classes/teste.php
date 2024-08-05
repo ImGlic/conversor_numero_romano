95972bc14f8028d20016c67dfd31ee1a68917287
Victor Glicério <66135800+ImGlic@users.noreply.github.com>
2024-08-05 09:46:18 -0300

    case teste

--- classes/teste.php
+++ classes/teste.php
@@ -1,6 +1,6 @@
 <?php
 
-function runTest($tipo, $value, $resultadoEsperado) {
+function testeConversao($tipo, $value, $resultadoEsperado) {
     $url = 'teste.php';
     $data = array('tipo' => $tipo, 'value' => $value);
 
@@ -20,27 +20,27 @@ function runTest($tipo, $value, $resultadoEsperado) {
     }
 }
 
-runTest('romanoParaDecimal', 'I', '<b style="color: #556B2F">1</b>');
-runTest('romanoParaDecimal', 'IV', '<b style="color: #556B2F">4</b>');
-runTest('romanoParaDecimal', 'IX', '<b style="color: #556B2F">9</b>');
-runTest('romanoParaDecimal', 'XL', '<b style="color: #556B2F">40</b>');
-runTest('romanoParaDecimal', 'XC', '<b style="color: #556B2F">90</b>');
-runTest('romanoParaDecimal', 'CD', '<b style="color: #556B2F">400</b>');
-runTest('romanoParaDecimal', 'CM', '<b style="color: #556B2F">900</b>');
-runTest('romanoParaDecimal', 'MCMXCIV', '<b style="color: #556B2F">1994</b>');
-runTest('romanoParaDecimal', 'MMMMMMMMMM', '<b style="color: #556B2F">10000</b>');
-runTest('romanoParaDecimal', 'INVALID', 'Número romano inválido: INVALID');
+testeConversao('romanoParaDecimal', 'I', '<b style="color: #556B2F">1</b>');
+testeConversao('romanoParaDecimal', 'IV', '<b style="color: #556B2F">4</b>');
+testeConversao('romanoParaDecimal', 'IX', '<b style="color: #556B2F">9</b>');
+testeConversao('romanoParaDecimal', 'XL', '<b style="color: #556B2F">40</b>');
+testeConversao('romanoParaDecimal', 'XC', '<b style="color: #556B2F">90</b>');
+testeConversao('romanoParaDecimal', 'CD', '<b style="color: #556B2F">400</b>');
+testeConversao('romanoParaDecimal', 'CM', '<b style="color: #556B2F">900</b>');
+testeConversao('romanoParaDecimal', 'MCMXCIV', '<b style="color: #556B2F">1994</b>');
+testeConversao('romanoParaDecimal', 'MMMMMMMMMM', '<b style="color: #556B2F">10000</b>');
+testeConversao('romanoParaDecimal', 'INVALID', 'Número romano inválido: INVALID');
 
-runTest('decimalParaRomano', '1', '<b style="color: #556B2F">I</b>');
-runTest('decimalParaRomano', '4', '<b style="color: #556B2F">IV</b>');
-runTest('decimalParaRomano', '9', '<b style="color: #556B2F">IX</b>');
-runTest('decimalParaRomano', '40', '<b style="color: #556B2F">XL</b>');
-runTest('decimalParaRomano', '90', '<b style="color: #556B2F">XC</b>');
-runTest('decimalParaRomano', '400', '<b style="color: #556B2F">CD</b>');
-runTest('decimalParaRomano', '900', '<b style="color: #556B2F">CM</b>');
-runTest('decimalParaRomano', '1994', '<b style="color: #556B2F">MCMXCIV</b>');
-runTest('decimalParaRomano', '10000', '<b style="color: #556B2F">MMMMMMMMMM</b>');
-runTest('decimalParaRomano', '0', 'Número fora do intervalo permitido (1-10000): 0');
-runTest('decimalParaRomano', '10001', 'Número fora do intervalo permitido (1-10000): 10001');
-runTest('decimalParaRomano', '-1', 'Número fora do intervalo permitido (1-10000): -1');
+testeConversao('decimalParaRomano', '1', '<b style="color: #556B2F">I</b>');
+testeConversao('decimalParaRomano', '4', '<b style="color: #556B2F">IV</b>');
+testeConversao('decimalParaRomano', '9', '<b style="color: #556B2F">IX</b>');
+testeConversao('decimalParaRomano', '40', '<b style="color: #556B2F">XL</b>');
+testeConversao('decimalParaRomano', '90', '<b style="color: #556B2F">XC</b>');
+testeConversao('decimalParaRomano', '400', '<b style="color: #556B2F">CD</b>');
+testeConversao('decimalParaRomano', '900', '<b style="color: #556B2F">CM</b>');
+testeConversao('decimalParaRomano', '1994', '<b style="color: #556B2F">MCMXCIV</b>');
+testeConversao('decimalParaRomano', '10000', '<b style="color: #556B2F">MMMMMMMMMM</b>');
+testeConversao('decimalParaRomano', '0', 'Número fora do intervalo permitido (1-10000): 0');
+testeConversao('decimalParaRomano', '10001', 'Número fora do intervalo permitido (1-10000): 10001');
+testeConversao('decimalParaRomano', '-1', 'Número fora do intervalo permitido (1-10000): -1');
 ?>
